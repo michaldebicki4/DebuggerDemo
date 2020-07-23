39e1bea9079cd9210365fa3a6ee6ceb08fc3c215
michaldebicki4 <michaldebicki4@gmail.com>
2020-07-23 14:52:19 +0200

    Initial commit

---
+++ src/com/michaldebicki/StringUtilities.java
@@ -0,0 +1,19 @@
+package com.michaldebicki;
+
+public class StringUtilities {
+    private StringBuilder sBuilder = new StringBuilder();
+    private int charsAdded = 0;
+
+    public void addChar(StringBuilder sBuilder, char c){
+        sBuilder.append(c);
+        charsAdded++;
+    }
+
+    public String upperAdPrefix(String str){
+        String upper = str.toUpperCase();
+        return "Prefix_" + upper;
+    }
+    public String addSuffix(String str){
+        return str + "__Suffix";
+    }
+}
